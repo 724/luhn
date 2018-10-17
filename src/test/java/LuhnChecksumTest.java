68d555bbdc4dad00a3fc922d8c68043ceff64ef9
Farhad <ifarhadfaghihi@gmail.com>
2018-10-17 10:09:03 +0330

    removed unused imports

--- src/test/java/LuhnChecksumTest.java
+++ src/test/java/LuhnChecksumTest.java
@@ -1,8 +1,6 @@
 import io.github.the724.LuhnChecksum;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 public class LuhnChecksumTest {
 
